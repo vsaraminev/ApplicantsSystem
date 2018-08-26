030a50e948e8f337fb392b3c3e5fb1d21d3000f6
vsaraminev <v.saraminev@abv.bg>
2018-08-26 19:27:03 +0300

    Add missing files

---
+++ ApplicantsSystem.Models/Status.cs
@@ -0,0 +1,18 @@
+﻿namespace ApplicantsSystem.Models
+{
+    using System.Collections.Generic;
+
+    public class Status
+    {
+        public Status()
+        {
+            this.Applicants = new List<AplicantStatus>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<AplicantStatus> Applicants { get; set; }
+    }
+}
